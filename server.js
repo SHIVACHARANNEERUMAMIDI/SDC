512a0773b63e82f1742ec1ce196985cd0c4aed97
SHIVACHARANNEERUMAMIDI <147908921+SHIVACHARANNEERUMAMIDI@users.noreply.github.com>
2025-04-05 00:05:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const __dirname = path.dirname(__filename);
 
 // Serve static files from 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
